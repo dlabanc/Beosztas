c5f78956b3f7a09185c91a924cd7bc05497adc89
jooedvard <jooedvard7@gmail.com>
2022-01-14 17:14:30 +0100

    mappaszerkezet

---
+++ global/Osztalyok.js
@@ -0,0 +1,97 @@
+class AlkalmazottTabla {
+    constructor(node, adat) {
+      this.node = node;
+      this.adat = adat;
+      this.nev = this.node.children("#nev");
+      this.beosztas = this.node.children("#beosztas");
+      this.lakcim = this.node.children("#lakcim");
+      this.elerhetoseg = this.node.children("#elerhetoseg");
+      this.email = this.node.children("#email");
+      this.setAdatok(this.adat);
+  
+  
+      this.node.on("click",()=>{
+        this.KattintasTrigger();
+      })
+  
+      this.node.on("mouseenter",()=>{
+        this.belepTrigger();
+      })
+  
+      this.node.on("mouseleave",()=>{
+        this.kilepTrigger();
+      })
+  
+      this.node.on("contextmenu",()=>{
+        this.jobbklikkTrigger();
+      })
+    }
+  
+    setAdatok(adat) {
+      this.adat = adat;
+      this.nev.text(adat.nev);
+      this.beosztas.text(adat.beosztas);
+      this.lakcim.text(adat.lakcim);
+      this.elerhetoseg.text(adat.elerhetoseg);
+      this.email.text(adat.email);
+    }
+  
+    KattintasTrigger() {
+      let esemeny = new CustomEvent("balklikk", { detail: this });
+      window.dispatchEvent(esemeny); //azért kell, hogy a script.js-ben lássuk
+    }
+  
+    belepTrigger() {
+      let esemeny = new CustomEvent("belep", { detail: this });
+      window.dispatchEvent(esemeny); //azért kell, hogy a script.js-ben lássuk
+    }
+  
+    kilepTrigger() {
+      let esemeny = new CustomEvent("kilep", { detail: this });
+      window.dispatchEvent(esemeny); //azért kell, hogy a script.js-ben lássuk
+    }
+  
+    jobbklikkTrigger() {
+      let esemeny = new CustomEvent("jobbklikk", { detail: this });
+      window.dispatchEvent(esemeny); //azért kell, hogy a script.js-ben lássuk
+    }
+}
+class Munkakor{
+    constructor(node, adat){
+        this.node=node;
+        this.adat=adat;
+        this.munkakorMegnevezes=this.node.children(".cimsor").children("h3");
+        this.munkakorKezdBetu=this.node.children(".cimsor").children(".circle").children("h2");
+        this.munkakorLeiras=this.node.children("p");
+        this.setAdat(this.adat);
+    }
+
+    setAdat(adat){
+        this.adat=adat;
+        this.munkakorMegnevezes.text(adat.megnevezes);
+        this.munkakorKezdBetu.text(adat.megnevezes.substring(0,1).toUpperCase());
+        this.munkakorLeiras.text(adat.leiras);
+    }
+
+    kattintasTrigger(gomb) {
+        let esemeny = new CustomEvent(gomb, {
+            detail: this.adat
+        });
+        window.dispatchEvent(esemeny);
+    }
+}
+class Muszak{
+    constructor(node, adat){
+        this.node=node;
+        this.adat=adat;
+        this.muszakTipus=this.node.children("h2");
+        this.muszakLeiras=this.node.children("p");
+        this.setAdat(this.adat);
+    }
+
+    setAdat(adat){
+        this.adat=adat;
+        this.muszakTipus.text(adat.tipus);
+        this.muszakLeiras.text(adat.leiras);
+    }
+}
